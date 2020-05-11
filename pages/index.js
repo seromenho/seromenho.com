49c6ef824764bca6fde9beb40e6cbefd380913c2
Ricardo Seromenho <ricardo.seromenho@gmail.com>
2020-05-11 22:45:34 +0100

    update social contacts style

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,41 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hey! I'm Ricardo Seromenho. I'm a hands on software engineer with a big passion for best practices, reusable patterns and scalable infrastructures that are built to last.</p>
-        <div className={utilStyles.social}><a href="https://www.linkedin.com/in/rseromenho/"><img height="32" height="32" src="/images/linkedin.svg" alt="LinkedIn"/></a><a href="https://twitter.com/rseromenho"><img height="32" height="32" src="/images/twitter.svg" alt="Twitter"/></a><a href="https://github.com/seromenho"><img height="32" height="32" src="/images/github.svg" alt="Github"/></a></div>
+        <p>
+         👋 Hey! I'm Ricardo Seromenho. I'm a hands on software engineer with a
+          big passion for best practices, reusable patterns and scalable
+          infrastructures that are built to last.
+        </p>
+        <div className={utilStyles.social}>
+          <a className={utilStyles.socialIcon} href="https://www.linkedin.com/in/rseromenho/">
+            <img
+              height="32"
+              height="32"
+              src="/images/linkedin.svg"
+              alt="LinkedIn"
+            />
+          </a>
+          <a className={utilStyles.socialIcon} href="https://twitter.com/rseromenho">
+            <img
+              height="32"
+              height="32"
+              src="/images/twitter.svg"
+              alt="Twitter"
+            />
+          </a>
+          <a className={utilStyles.socialIcon} href="https://github.com/seromenho">
+            <img
+              height="32"
+              height="32"
+              src="/images/github.svg"
+              alt="Github"
+            />
+          </a>
+        </div>
+        <div className={utilStyles.center}>
+          <p>© Ricardo Seromenho {new Date().getFullYear()}</p>
+        </div>
       </section>
     </Layout>
-  )
+  );
 }
